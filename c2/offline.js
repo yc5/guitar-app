39887966905687b34a509b6d969a3343e14c9499
yc5 <yc@xstar.me>
2021-11-04 03:57:56 +0800

    init

---
+++ c2/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1545765374,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/g-sheet0.png",
+		"images/s1-sheet0.png",
+		"images/s5-sheet0.png",
+		"images/s2-sheet0.png",
+		"images/s3-sheet0.png",
+		"images/s4-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
